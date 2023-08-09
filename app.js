ceee883baad81088a087e706fe0547199e94bc26
alx <kom.alx.frlv@gmail.com>
2023-08-09 11:03:17 +0500

    sosa

--- app.js
+++ app.js
@@ -35,9 +35,6 @@ mqttClient.on("message", function (topic, payload, packet) {
   var getTopic = topic.split("/");   //  Получаем топики
   //var getSend = payload.toString();
 
-  console.log("Topics - " + getTopic);
-  //console.log("payload - " + (getSend));
-
   // var getSend = JSON.parse(payload.toString()); //  Получаем сообщение 
 
   // is.emit(getSend.message, {getSend: getSend, getTopic: getTopic})
@@ -46,36 +43,27 @@ mqttClient.on("message", function (topic, payload, packet) {
       Берем из базы инфоу о том , кому прниаждлежит шлюз 
   */
   // отправляем в соотвествующую  комнату    
-  //is.to(login).emit("cmd", '{"payload":[ ' + JSON.stringify(getSend) + '], "topic" : [' + JSON.stringify(getTopic) + "]}");
+  is.to(getTopic[0]).emit("cmd", '{"payload":[ ' + JSON.stringify(getSend) + '], "topic" : [' + JSON.stringify(getTopic) + "]}");
 
 });
 
 is.on("connection", function (socket) {
   var ID = socket.id.toString().substr(0, 5);
-  //socket.join(ID);
-
   console.log("connect_USER: " + ID);
 
   // auth для захода в сокет комнату по какому то типу данных (В данном случае по логину)
 
   socket.on("auth", function (data) {
-    var login = data.user;
-    let uid = data.uid; // Под вопросом 
-    socket.join(login); // входим сокетами в комнату юзвера 
-    console.log("Получаем шлюз  с логина -   - " + login + " - ");
-    /*
-        Берем login юзверу и из базы берем id шлюза ,который ему принадлежит (have)
-    */
+    let room = data.room; // Под вопросом 
+    socket.join(room); // входим сокетами в комнату юзвера 
     console.log("get + device " + have); // have - это id шлюза , который принадлежит юзверу  
-    mqttClient.publish(uid + "/" + have + "/bridge/config/devices/get", ""); // Получаем данные шлюза
-
+    mqttClient.publish(room + "/" + have + "/bridge/config/devices/get", ""); // Получаем данные шлюза
   });
 
   socket.on("permit_join", function (data) {
     let id = data.user;
     let uid = data.uid;
     //socket.join(login);
-
     console.log(ID + " - permit_join - from id - ");
     mqttClient.publish(uid + "/" + id + "/bridge/config/permit_join", '{"permit_join ": true}'); // Включаем сопряжение 
   });
@@ -102,13 +90,13 @@ is.on("connection", function (socket) {
   });
 
   socket.on("broker", function (data) {         //  Все сообщения к брокеру отправлять в это простарнство имен 
-                                           //
     let userId = ""+data.userId;             //
     let gatewayId = ""+data.gatewayId;      //  Формиурем топик 
     let device = ""+data.device;           //
-                                       //
     let message = ""+data.message;       //  Основной json , который собирается в фронте и отправляется к брокеру 
+
     console.log("{ onBroker: " + userId + "/" + gatewayId + "/" + device + "\n" + "message: " + message + " }");
+    
     mqttClient.publish(userId + "/" + gatewayId + "/" + device, message); // Отправлем комманду на шлюз с айди устройством 
   });
 
