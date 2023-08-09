acbfd8d5a68bce5d67c6784abf94a8dcf3b21946
alx <kom.alx.frlv@gmail.com>
2023-08-09 17:51:38 +0500

    gavnormal

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const application = express();
-var fs = require("fs");
 
 const mqtt = require("mqtt");
 
 const http = require("http").Server(application);
 const is = require("socket.io")(http);
+const io = require('socket.io-client');
+const ioClient = io.connect('http://localhost:5002')
 
-//const { db } = require('./src/db');
+const { db } = require('./src/db');
 
 const mqttUrl = "mqtt://hs.k-telecom.org:8883";
 const mqttOptions = {
@@ -24,81 +25,31 @@ const mqttClient = mqtt.connect(mqttUrl, mqttOptions);
 
 mqttClient.on("connect", function () {
   if (mqttClient.connected) {
-    console.log("conected");
+    //console.log("conected");
     mqttClient.subscribe("#");  // Подпись на все топики
   } else {
     console.log("disconeted");
   }
 });
 
-mqttClient.on("message", function (topic, payload, packet) {
-  // Payload is Buffer
-  var getTopic = topic.split("/");   //  Получаем топики
-  //var getSend = payload.toString();
-  console.log("getTopic = " + payload.toString());
-  // var getSend = JSON.parse(payload.toString()); //  Получаем сообщение 
-
-  // is.emit(getSend.message, {getSend: getSend, getTopic: getTopic})
-  try {
-    let obj = JSON.parse(payload.toString())
-
-    if (obj.modeTelecom) {
-      is.emit("auth", '{"payload":[ ' + payload.toString() + '], "topic" : [' + JSON.stringify(getTopic) + "]}");
-      console.log("gavnormal = " + obj['modeTelecom']);
-    }
-  } catch (e) {
-    console.log('oshibka: Error parsing')
-  }
-
-  /* 
-      Берем из базы инфоу о том , кому прниаждлежит шлюз 
-  */
-  // отправляем в соотвествующую  комнату    
-  is.to(getTopic[0]).emit("cmd", '{"payload":[ ' + payload.toString() + '], "topic" : [' + JSON.stringify(getTopic) + "]}");
-
-});
+const uuid = require("uuid");
+const apple = uuid.v4();
 
 is.on("connection", function (socket) {
+
+  socket.join("server");
+
   var ID = socket.id.toString().substr(0, 5);
+
   console.log("connect_USER: " + ID);
 
-  socket.on("saveToDb", function (data) {
-    console.log("pizdata: " + data);
-  });
-
-  // auth для захода в сокет комнату по какому то типу данных (В данном случае по логину)
 
   socket.on("auth", function (data) {
-    let room = data.room; // Под вопросом 
-    socket.join(room); // входим сокетами в комнату юзвера
-    console.log(data);
-    console.log("get + device " + data.gatewayId); // have - это id шлюза , который принадлежит юзверу  
-    mqttClient.publish(room + "/" + data.gatewayId + "/bridge/config/devices/get", ""); // Получаем данные шлюза
-  });
+    let room = data.room;
 
-  socket.on("permit_join", function (data) {
-    let id = data.user;
-    let uid = data.uid;
-    //socket.join(login);
-    console.log(ID + " - permit_join - from id - ");
-    mqttClient.publish(uid + "/" + id + "/bridge/config/permit_join", '{"permit_join ": true}'); // Включаем сопряжение 
-  });
+    socket.join(room);
 
-  socket.on("delete_device", function (data) {
-    let id = data.user;
-    let device = data.device;
-    let uid = data.uid;
-
-    console.log(ID + " - delete_divece -" + id);
-    mqttClient.publish(uid + "/" + id + "/bridge/config/force_remove", device); // Удаляем девайс по иди
-  });
-
-  // Создание нового датчиика 
-  socket.on("connected", async function (data) {
-    console.log("Создание датчика: " + data);
-    await db.sensor.create({
-      data: data
-    });
+    mqttClient.publish(room + "/" + data.gatewayId + "/bridge/config/devices/get", "");
   });
 
   socket.on("disconnect", function () {
@@ -115,11 +66,37 @@ is.on("connection", function (socket) {
 
     mqttClient.publish(userId + "/" + gatewayId + "/" + device, message); // Отправлем комманду на шлюз с айди устройством 
   });
+});
 
+ioClient.on('saveToDb', function (data) {
+  console.log('SAVE TO DB:', data);
 });
-is.on("saveToDb", function (data) {
-  console.log("abdoolla = " + data);
+
+
+mqttClient.on("message", function (topic, payload, packet) {
+  // Payload is Buffer
+  var getTopic = topic.split("/");   //  Получаем топики
+  var getSend = payload.toString();
+  //console.log("getTopic = " + payload.toString());
+  // var getSend = JSON.parse(payload.toString()); //  Получаем сообщение 
+
+  // is.emit(getSend.message, {getSend: getSend, getTopic: getTopic})
+  try {
+    let obj = JSON.parse(payload.toString())
+
+    if (obj.modeTelecom) {
+      is.emit("saveToDb", obj['data']);
+      console.log("gavnormal = " + obj['modeTelecom']);
+    }
+  } catch (e) {
+    //console.log('oshibka: Error parsing')
+  }
+
+  is.to(getTopic[0]).emit("cmd",'{"payload":[ ' + payload.toString +'], "topic" : [' + getTopic + "]}");
 });
+
+
+
 http.listen(5002, function () {
   console.log("Клиентский канал запущен, порт: " + 5002);
 });
